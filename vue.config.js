d45ec80ec022841d170e8d408b128a334d062514
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2020-04-24 09:47:21 -0300

    add build configuration for windows in vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+  module.exports = {
+    pluginOptions: {
+      electronBuilder: {
+        builderOptions: {
+          // options placed here will be merged with default configuration and passed to electron-builder
+            "productName": "ESP32 Flasher esptool.py GUI",
+            "appId": "com.bauertronix.esp32flasher",
+            "win": {
+                "target": ["portable"],
+                "icon": "build/flash.ico"
+            },
+            "portable": {
+                "artifactName": "esp32_flasher_esptool.exe"
+            },
+            // "directories": {
+            //     "output": "electron/output",
+            //     "app": "electron/app",
+            //     "buildResources": "electron/buildResources"
+            // }
+        }
+      }
+    }
+}
