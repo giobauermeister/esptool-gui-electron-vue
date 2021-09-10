5d7dddb4ab1802604656f6b1a11f839bf340112b
r3n33 <renee1382@gmail.com>
2021-09-10 17:25:06 -0500

    Catch SerialPort list() errors

--- src/components/scan_serial_ports.js
+++ src/components/scan_serial_ports.js
@@ -1,19 +1,25 @@
 const SerialPort = require('serialport')
 
 export default async function scanSerialPorts (mainWindow) {
-  var portList = await SerialPort.list()
-  //console.log(portList)
-
-  let validPorts = portList.filter((port) => {
-    return port.manufacturer === 'FTDI' || port.manufacturer === 'Silicon Labs'
+  var portList = await SerialPort.list().catch((err) => {
+    console.log('SerialPort.list(): error: ')
+    console.log(err)
+    mainWindow.webContents.send('validComPorts', []);
   })
-  //console.log(validPorts)
+  if (portList != undefined) {
+    console.log(portList)
 
-  let result = [];
-  for (var i=0; i < validPorts.length; ++i) {
-    result.push({port:validPorts[i].path});
+    let validPorts = portList.filter((port) => {
+      return port.manufacturer === 'FTDI' || port.manufacturer === 'Silicon Labs'
+    })
+    console.log(validPorts)
+
+    let result = [];
+    for (var i=0; i < validPorts.length; ++i) {
+      result.push({port:validPorts[i].path});
+    }
+    console.log(result)
+
+    mainWindow.webContents.send('validComPorts', result);
   }
-  //console.log(result)
-
-  mainWindow.webContents.send('validComPorts', result);
 }
